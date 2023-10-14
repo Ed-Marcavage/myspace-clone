450324d59a2d5765ec429680bc196136766f98ca
Ed-Marcavage <edmarcavage256@proton.me>
2023-10-13 23:57:04 -0400

    INIT Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import NavMenu from './NavMenu';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +14,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <NavMenu />
+      {children}
+    </>
   )
 }
