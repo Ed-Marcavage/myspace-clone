527ed717cba9716cbe1fda5b2159475ba9af269f
TuringComplete123 <TuringComplete123@proton.me>
2023-10-29 22:31:33 -0400

    Init SSR

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image'
 
 export default function Home() {
+  // console.log hello
+  // print hello world 
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
     </main>
   )
 }
